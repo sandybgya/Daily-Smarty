03743efb2689d090fb51f14176f6d1cd6fc6fa2b
sandybgya <anderberg.sandy@clarendonisd.net>
2020-05-16 21:43:36 -0500

    flask backend

--- Desktop/untitled folder/programming stuff/stuff from prework first/Daily Smarty/DailySmartyUI/src/components/app.js
+++ Desktop/untitled folder/programming stuff/stuff from prework first/Daily Smarty/DailySmartyUI/src/components/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Logo from "./logo";
 
 export default class App extends Component {
   render() {
@@ -6,6 +7,7 @@ export default class App extends Component {
       <div>
         <div>
           logo
+          <Logo/>
         </div>
       </div>
     );
---
+++ Desktop/untitled folder/programming stuff/stuff from prework first/Daily Smarty/DailySmartyUI/src/components/logo.js
@@ -0,0 +1,13 @@
+import React,  { Component } from "react";
+
+class logo extends Component {
+    render() {
+        return (
+            <div className="logo-main">
+                <img alt="daily smarty ui image logo big" src="/assets/download.png"/>
+            </div>
+        )
+    }
+}
+
+export default logo;
